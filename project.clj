987b6f57d0888d9fb52acd2dea792f9942a8b8f6
Leonard Mbuli <lm.mbuli@gmail.com>
2011-03-22 11:44:25 +0200

    got links working and added a textbox to home page

--- project.clj
+++ project.clj
@@ -1,4 +1,24 @@
 (defproject Rheo_docs "1.0.0-SNAPSHOT"
-  :description "FIXME: write"
+  :description "documentation system"
   :dependencies [[org.clojure/clojure "1.2.0"]
-                 [org.clojure/clojure-contrib "1.2.0"]])
+                 [org.clojure/clojure-contrib "1.2.0"]
+                 [compojure "0.6.0-RC4"]
+               ;;  [ring/ring-core "0.3.1"]
+               ;;  [ring/ring-servlet "0.3.1"]
+                 [hiccup "0.3.0"]
+                 [congomongo "0.1.3-SNAPSHOT"]
+                 [clj-time "0.3.0-SNAPSHOT"]
+                 [org.clojars.hozumi/mongodb-session "1.0.1"]
+                 ]
+  :dev-dependencies [[ring/ring-jetty-adapter "0.3.1"]                    
+                     [swank-clojure "1.2.1"]
+                     [ring/ring-devel "0.3.1"]
+                     [lein-ring "0.3.2"]
+                     [uk.org.alienscience/leiningen-war "0.0.8"]
+                     ]
+  ;; Used by leiningen-war to deploy static resources
+  :web-content "public"
+  ;; The servlet used by Tomcat
+  ;;:aot [clj-rheo.servlet]
+  :ring {:handler Rheo_docs.core/app}
+            )
--- src/Rheo_docs/core.clj
+++ src/Rheo_docs/core.clj
@@ -1,1 +1,29 @@
-(ns Rheo_docs.core)
+(ns Rheo_docs.core
+  (:use compojure.core)
+  (:use hiccup.core)
+  (:use hiccup.form-helpers)
+  (:require [compojure.route :as route]
+            [compojure.handler :as handler])
+  )
+
+(def app
+  (handler/site main-routes)
+  )
+
+(defn greet
+      ([] (html (form-to [:POST "/"] 
+                          (text-field :name "CoolKid"))))
+      ([name] (html 
+                [:p (str "Hello " name "!" )]
+                [:a {:href "/"} "Home"]))
+      )
+
+(defroutes main-routes
+           (GET "/" [] (greet))
+           (GET "/:igama" [igama] (greet igama))
+           (POST "/" [name] (greet name))
+           (route/resources "/")
+           (route/not-found "PAGE NOT FOUND")
+           )
+
+
