44fe613a67b870cf8c59bb20f648b13cd704633b
Mandla Mbuli <lm.mbuli@gmail.com>
2011-06-11 09:35:24 +0200

    made project.clj look the the webapp's

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject ryulogy "1.0.0-SNAPSHOT"
+(defproject ryulogy "11.06"
   :description "documentation system"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
@@ -8,17 +8,14 @@
                  [hiccup "0.3.0"]
                  [congomongo "0.1.3-SNAPSHOT"]
                  [clj-time "0.3.0-SNAPSHOT"]
-                 [org.clojars.hozumi/mongodb-session "1.0.1"]
-                 ]
+                 [org.clojars.hozumi/mongodb-session "1.0.1"]]
   :dev-dependencies [[ring/ring-jetty-adapter "0.3.1"]                    
                      [swank-clojure "1.2.1"]
                      [ring/ring-devel "0.3.1"]
                      [lein-ring "0.2.4"]
-                     [uk.org.alienscience/leiningen-war "0.0.8"]
-                     ]
+                     [uk.org.alienscience/leiningen-war "0.0.8"]]
   ;; Used by leiningen-war to deploy static resources
-  :web-content "public"
+  ;; :web-content "public"
   ;; The servlet used by Tomcat
-  ;;:aot [clj-rheo.servlet]
-  :ring {:handler ryulogy.core/app}
-            )
+  :aot [clj-rheo.servlet]
+  :ring {:handler ryulogy.core/app})
